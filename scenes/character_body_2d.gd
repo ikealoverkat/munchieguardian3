9b6f32abb8e043260e071cda488bab4c0cc3b337
kat <katwang7749@gmail.com>
2026-01-04 03:38:53 -0500

    fix janky movement and add scale state code

--- scenes/character_body_2d.gd
+++ scenes/character_body_2d.gd
@@ -3,13 +3,13 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -250.0
-const RUN_SCALE = 0.08
-const JUMP_SCALE = 0.1
-const IDLE_SCALE = 0.075
+const RUN_SCALE = Vector2(0.08, 0.08)
+const JUMP_SCALE = Vector2(0.1, 0.1)
+const IDLE_SCALE = Vector2(0.075, 0.075)
 
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		$munchieguardianSprite.play("jumping")
 		
@@ -20,37 +20,40 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		$munchieguardianSprite.play("jumping")
-		$munchieguardianSprite.scale = Vector2(JUMP_SCALE, JUMP_SCALE)
-		$CollisionShape2D.scale = Vector2(JUMP_SCALE, JUMP_SCALE)
+		set_scale_for_state("jump")
 		
-		if direction == 1.0:
-			$munchieguardianSprite.flip_h = false
-			$Staff.flip_h = false
-			
-		if direction == -1.0:
-			$munchieguardianSprite.flip_h = false
-			$Staff.flip_h = false
+		if direction != 0:
+			velocity.x = direction * 0.8 * SPEED
+			update_sprite_direction(direction)
+	
 	else:
-		if direction == 1.0:
+		if direction != 0:
 			velocity.x = direction * SPEED
 			$munchieguardianSprite.play("running")
-			$munchieguardianSprite.flip_h = false
-			$Staff.flip_h = false
-			$munchieguardianSprite.scale = Vector2(RUN_SCALE, RUN_SCALE)
-			$CollisionShape2D.scale = Vector2(RUN_SCALE, RUN_SCALE)
-			
-		elif direction == -1.0:
-			velocity.x = direction * SPEED
-			$munchieguardianSprite.play("running")
-			$munchieguardianSprite.flip_h = true
-			$Staff.flip_h = true
-			$munchieguardianSprite.scale = Vector2(RUN_SCALE, RUN_SCALE)
-			$CollisionShape2D.scale = Vector2(RUN_SCALE, RUN_SCALE)
+			set_scale_for_state("run")
+			update_sprite_direction(direction)
 			
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			$munchieguardianSprite.play("idle")
-			$munchieguardianSprite.scale = Vector2(IDLE_SCALE, IDLE_SCALE)
-			$CollisionShape2D.scale = Vector2(IDLE_SCALE, IDLE_SCALE)
+			set_scale_for_state("idle")
 
 	move_and_slide()
+
+func update_sprite_direction(direction: float) -> void:
+	var should_flip = direction < 0 #should flip when facing left
+	$munchieguardianSprite.flip_h = should_flip
+	$Staff.flip_h = should_flip
+
+func set_scale_for_state(state: String) -> void:
+	match state:
+		"run":
+			$munchieguardianSprite.scale = RUN_SCALE
+			$CollisionShape2D.scale = RUN_SCALE
+		"idle":
+			$munchieguardianSprite.scale = IDLE_SCALE
+			#$CollisionShape2D.scale = IDLE_SCALE
+		"jump":
+			$munchieguardianSprite.scale = JUMP_SCALE
+			#$CollisionShape2D.scale = JUMP_SCALE			
+	
